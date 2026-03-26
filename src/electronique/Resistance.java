69b809fcdb6d90aba7e647c48cfce283792ee44c
Alexis Arteau <2239212@cegeplimoilou.ca>
2026-03-26 14:03:20 -0400

    Ajout de la version de base de Resistance.java

--- src/electronique/Resistance.java
+++ src/electronique/Resistance.java
@@ -3,14 +3,27 @@ package electronique;
 public class Resistance extends Composant {
     private double resistance;
 
-    public Resistance(double resistance)
+    public Resistance(double resistance) throws IllegalArgumentException
     {
+        setResistance(resistance);
+    }
+
+    public void setResistance(double resistance) throws IllegalArgumentException
+    {
+        if (!isValidResistance(resistance))
+            throw new IllegalArgumentException("resistance est invalide");
+
         this.resistance = resistance;
+    }
+
+    private static boolean isValidResistance(double resistance)
+    {
+        return resistance > 0;
     }
 
     @Override
     public double calculerResistance()
     {
-        return 0.0D;
+        return this.resistance;
     }
 }
