580685d30ba7d10c9e5265f06a68e63e3cf28308
AlexisArteau <2239212@cegeplimoilou.ca>
2026-04-01 10:03:08 -0400

    Ajout de la fonctionnalité de lecture des circuits JSON

--- src/app/CircuitBuilder.java
+++ src/app/CircuitBuilder.java
@@ -1,21 +1,66 @@
 package app;
 
-import electronique.Composant;
-import org.w3c.dom.Node;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import electronique.*;
 
-public class CircuitBuilder {
-    public CircuitBuilder()
-    {
+import java.io.File;
+import java.util.ArrayList;
 
+public abstract class CircuitBuilder {
+    public static Composant readCircuitFromFile(String circuitFilePath) {
+        ObjectMapper objMapper = new ObjectMapper();
+
+        Composant circuit = null;
+
+        try {
+            JsonNode parentNode = objMapper.readTree(new File(circuitFilePath));
+
+            JsonNode circuitNode = parentNode.get("circuit");
+
+            circuit = parseNode(circuitNode);
+        } catch (Exception ex) {
+            IO.print(ex.getMessage());
+        }
+
+        return circuit;
     }
 
-    public static Composant construireCircuit(String chaine)
+    private static Composant parseNode(JsonNode currentNode)
     {
-        return null;
-    }
+        final String type = currentNode.get("type").asText();
 
-    private Composant lireComposant(Node node)
-    {
-        return null;
+        Composant composant = null;
+
+        switch(type)
+        {
+            case "parallele", "serie":
+                Circuit circuit = null;
+
+                if (type.equals("parallele"))
+                    circuit = new CircuitParallele();
+
+                if (type.equals("serie"))
+                    circuit = new CircuitSerie();
+
+                JsonNode composantsNode = currentNode.get("composants");
+
+                for (JsonNode compNode : composantsNode)
+                    circuit.addComposant(parseNode(compNode));
+
+                composant = circuit;
+
+                break;
+            case "resistance":
+                final double resistanceVal = currentNode.get("valeur").asDouble();
+
+                composant = new Resistance(resistanceVal);
+
+                break;
+            default:
+                throw new RuntimeException("unexcepted composant");
+        }
+
+        return composant;
     }
 }
