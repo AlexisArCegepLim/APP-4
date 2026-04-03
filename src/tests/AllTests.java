4f34187639e4ea37b2713b6ede60f90f80c2fb4c
Alexis Arteau <2239212@cegeplimoilou.ca>
2026-04-03 10:59:06 -0400

    Ajout de tests generales pour couvrir la majorite du code

---
+++ src/tests/AllTests.java
@@ -0,0 +1,141 @@
+package tests;
+
+import app.CLIEntryPoint;
+import app.ChoixUtilisateur;
+import app.CircuitBuilder;
+import electronique.CircuitParallele;
+import electronique.CircuitSerie;
+import electronique.Resistance;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllTests {
+    @Test
+    public void circuitZoneNord()
+    {
+        CircuitSerie serie2 = new CircuitSerie();
+
+        serie2.addComposant(new Resistance(5.0));
+        serie2.addComposant(new Resistance(20.0));
+
+        CircuitParallele parallele = new CircuitParallele();
+
+        parallele.addComposant(serie2);
+        parallele.addComposant(new Resistance(10));
+        parallele.addComposant(new Resistance(5));
+
+        CircuitSerie serie1 = new CircuitSerie();
+
+        serie1.addComposant(new Resistance(10.0));
+        serie1.addComposant(parallele);
+
+        assertEquals(12.941176470588236, serie1.calculerResistance());
+    }
+
+    @Test
+    public void circuitPublicQuartier()
+    {
+        CircuitSerie serie1 = new CircuitSerie();
+
+        serie1.addComposant(new Resistance(2000));
+        serie1.addComposant(new Resistance(4750));
+
+        CircuitSerie serie2 = new CircuitSerie();
+
+        serie2.addComposant(new Resistance(1100));
+        serie2.addComposant(new Resistance(1500));
+
+        CircuitParallele para = new CircuitParallele();
+
+        para.addComposant(serie1);
+        para.addComposant(serie2);
+
+        assertEquals(1877.0053475935829, para.calculerResistance());
+    }
+
+    @Test
+    public void circuitSecoursHopital()
+    {
+        CircuitParallele para = new CircuitParallele();
+
+        para.addComposant(new Resistance(10000));
+        para.addComposant(new Resistance(2000));
+        para.addComposant(new Resistance(1000));
+
+        assertEquals(625.0, para.calculerResistance());
+    }
+
+    @Test
+    public void filesListCircuits() {
+        List<Path> cheminsCircuitsJSON = null;
+
+        try {
+            cheminsCircuitsJSON = Files.list(CLIEntryPoint.dossierCircuitsJSON).toList();
+        } catch(Exception ex) {
+            fail(ex.getMessage());
+        }
+
+        assertEquals(3, cheminsCircuitsJSON.size());
+        assertEquals("complexe_industriel_zone_nord.json", cheminsCircuitsJSON.get(0).getFileName().toString());
+        assertEquals("eclairage_public_quartier.json", cheminsCircuitsJSON.get(1).getFileName().toString());
+        assertEquals("reseau_secours_hopital.json", cheminsCircuitsJSON.get(2).getFileName().toString());
+    }
+
+    @Test
+    public void circuitJSONResEqValide()
+    {
+        assertEquals(12.941176470588236, CircuitBuilder.chargerCircuit(Paths.get("C:\\Users\\Froggy\\Desktop\\app_4_repo_local\\src\\donnees\\complexe_industriel_zone_nord.json")).calculerResistance());
+        assertEquals(1877.0053475935829, CircuitBuilder.chargerCircuit(Paths.get("C:\\Users\\Froggy\\Desktop\\app_4_repo_local\\src\\donnees\\eclairage_public_quartier.json")).calculerResistance());
+        assertEquals(625.0, CircuitBuilder.chargerCircuit(Paths.get("C:\\Users\\Froggy\\Desktop\\app_4_repo_local\\src\\donnees\\reseau_secours_hopital.json")).calculerResistance());
+    }
+
+    @Test
+    public void resistance()
+    {
+        assertThrows(RuntimeException.class, () -> new Resistance(-1));
+        assertThrows(RuntimeException.class, () -> new Resistance(3).setResistance(-1));
+
+        Resistance r = new Resistance(10);
+
+        assertEquals(10, r.calculerResistance());
+
+        assertThrows(RuntimeException.class, () -> r.setResistance(0));
+
+        assertEquals(10, r.calculerResistance());
+    }
+
+    @Test
+    public void circuitEnGenerale()
+    {
+        assertThrows(RuntimeException.class, ()->new CircuitSerie().addComposant(new CircuitSerie()));
+
+        CircuitSerie serie = new CircuitSerie();
+
+        serie.addComposant(new Resistance(100));
+        serie.addComposant(new Resistance(200));
+
+        assertEquals(100, serie.getComposant(0).calculerResistance());
+        assertEquals(200, serie.getComposant(1).calculerResistance());
+        assertThrows(RuntimeException.class, ()->serie.getComposant(-1));
+        assertThrows(RuntimeException.class, ()->serie.getComposant(2));
+        assertEquals(2, serie.getListeComposants().size());
+        assertEquals(100, serie.getListeComposants().get(0).calculerResistance());
+    }
+
+    @Test
+    public void inputUtilisateur() {
+        assertEquals(ChoixUtilisateur.RELANCER, ChoixUtilisateur.charToChoix('R'));
+        assertEquals(ChoixUtilisateur.QUITTER, ChoixUtilisateur.charToChoix('Q'));
+        assertEquals(ChoixUtilisateur.INVALIDE, ChoixUtilisateur.charToChoix('d'));
+        assertEquals(ChoixUtilisateur.INVALIDE, ChoixUtilisateur.charToChoix('r'));
+        assertEquals(ChoixUtilisateur.INVALIDE, ChoixUtilisateur.charToChoix('q'));
+        assertEquals(ChoixUtilisateur.INVALIDE, ChoixUtilisateur.charToChoix('_'));
+        assertEquals(ChoixUtilisateur.INVALIDE, ChoixUtilisateur.charToChoix('1'));
+    }
+}
